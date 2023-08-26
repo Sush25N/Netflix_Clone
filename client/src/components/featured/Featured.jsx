534afaab824b92ee4059efac40cc2dac93a3578d
Sush25N <sushgabhane123@gmail.com>
2023-08-26 19:53:03 +0530

    added list structure

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -5,9 +5,29 @@ import dummy_movie_name from "../../assets/dummy_movie_name.png"
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 
-const Featured = () => {
+const Featured = ({ type }) => {
   return (
     <div className='featured'>
+      {type && (
+        <div className='category'>
+          <span>{type === "movie" ? "Movies" : "TVshows"}</span>
+          <select name="genre" id="genre">
+            <option>Genre</option>
+            <option value="adventure">Adventure</option>
+            <option value="comedy">Comedy</option>
+            <option value="crime">Crime</option>
+            <option value="fantasy">Fantasy</option>
+            <option value="historical">Historical</option>
+            <option value="horror">Horror</option>
+            <option value="romance">Romance</option>
+            <option value="thriller">Thriller</option>
+            <option value="western">Western</option>
+            <option value="animation">Animation</option>
+            <option value="drama">Drama</option>
+            <option value="documentary">Documentary</option>
+          </select>
+        </div>
+      )}
       <img src={dummy_pic} alt=""/>
       <div className="info">
         <img src={dummy_movie_name} alt="" />
